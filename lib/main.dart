42246e4509125669692966f8a7ee228997292aaf
Mohammad Ebbeni <colonal.ch@gmail.com>
2022-07-26 00:45:09 +0300

    Code Refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'core/widget/helper/screen_helper.dart';
-import 'features/home/data/datasources/home_remote_data_source.dart';
-import 'features/home/data/repositories/home_repositories_impl.dart';
 import 'main_screen.dart';
 import 'core/theme/app_thime.dart';
 
@@ -36,74 +33,6 @@ class MyApp extends StatelessWidget {
             ? AppTheme.themeDesktop
             : AppTheme.themeMobile,
         home: const MainScreen(),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getD();
-  }
-
-  void getD() async {
-    final m = await HomeRepositoriesImpl(
-            remoteDataSource:
-                HomeRemoteDataSourceImpl(firebase: FirebaseFirestore.instance))
-        .getHomeData();
-    m.fold((failuer) {
-      debugPrint("Failuer");
-      debugPrint(failuer.toString());
-    }, (posts) {
-      debugPrint("Successes");
-      debugPrint(posts.name);
-      debugPrint(posts.career);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
